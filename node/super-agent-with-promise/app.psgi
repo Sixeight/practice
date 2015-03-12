f6337d582dc3a79c770dc6cebf55c7102e576ce3
Tomohiro Nishimura <tomohiro68@gmail.com>
2015-03-13 02:11:21 +0900

    Write learning code of fetch

--- node/super-agent-with-promise/app.psgi
+++ node/super-agent-with-promise/app.psgi
@@ -2,17 +2,35 @@ use strict;
 use warnings;
 
 use Plack::Builder;
+use Plack::Request;
+
+use JSON::XS ();
+use Data::Dumper;
+
+my $json = JSON::XS->new;
 
 open my $fh, "<", "index.html";
-my $body = eval { local $/; <$fh> };
+my $html = eval { local $/; <$fh> };
 
 my $app = sub {
   my $env = shift;
-  [
-    200,
-    ["Content-Type" => "text/html"],
-    [$body],
-  ]
+  my $req = Plack::Request->new($env);
+  my $res = $req->new_response(200);
+  if ($req->method eq "POST") {
+    my $fh = $req->body;
+    my $body = eval { local $/; <$fh> };
+    print STDERR "===========\n";
+    print STDERR Data::Dumper->Dump([$req->parameters]);
+    print STDERR "===========\n";
+    print STDERR $body, "\n";
+    print STDERR "===========\n";
+    $res->content_type("application/json");
+    $res->content($json->encode({a => 1}));
+  } else {
+    $res->content_type("text/html");
+    $res->content($html);
+  }
+  $res->finalize;
 };
 
 builder {
--- node/super-agent-with-promise/src/app.js
+++ node/super-agent-with-promise/src/app.js
@@ -1,10 +1,61 @@
 var promiseagent = require("./promiseagent.js")
+require("whatwg-fetch");
 
 promiseagent
   .get("./dist/test.json")
   .promise()
   .then(function(res) {
     console.log(res.body);
+  })
+  .catch(function(err) {
+    console.log(err.message);
+  });
+
+fetch("./dist/test.json")
+  .then(function(res) {
+    if (res.status == 200) {
+      return Promise.resolve(res);
+    } else {
+      return Promise.reject(new Error(res.statusText));
+    }
+  })
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(json) {
+    console.log(json);
+  }).catch(function(err) {
+    console.log(err.message);
+  });
+
+var formData = new FormData();
+formData.append("a", 1);
+formData.append("b", "string");
+
+fetch("/", {
+    method: "POST",
+    body: formData
+  })
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(json) {
+    console.log(json);
+  })
+  .catch(function(err) {
+    console.log(err.message);
+  });
+
+
+fetch("/", {
+    method: "POST",
+    body: "a=5"
+  })
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(json) {
+    console.log(json);
   })
   .catch(function(err) {
     console.log(err.message);
